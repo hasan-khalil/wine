1fe81710f41aff95779e0397286a04f1996a7536
cudi1999 <42251302+cudi1999@users.noreply.github.com>
2020-08-14 07:08:59 +0300

    Update installwine.sh

--- installwine.sh
+++ installwine.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo"install wine for linux"
 
-#sudo dpkg --add-architecture i386
-#sudo apt-get update
-#sudo apt-get install wine:i386
-#sudo apt-get install wine-bin:i386
-#sudo dpkg --add-architecture i386 && apt-get update && apt-get install wine-bin:i386
+sudo dpkg --add-architecture i386
+sudo apt-get update
+sudo apt-get install wine:i386
+sudo apt-get install wine-bin:i386
+sudo dpkg --add-architecture i386 && apt-get update && apt-get install wine-bin:i386
